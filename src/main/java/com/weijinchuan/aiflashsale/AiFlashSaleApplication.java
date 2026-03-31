9537945636d2c0daab92fe907e690c6277d2ac4a
CodeShy <2678632406@qq.com>
2026-03-31 11:32:47 +0800

    feat:项目初始化

---
+++ src/main/java/com/weijinchuan/aiflashsale/AiFlashSaleApplication.java
@@ -0,0 +1,13 @@
+package com.weijinchuan.aiflashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFlashSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiFlashSaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weijinchuan/aiflashsale/AiFlashSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weijinchuan.aiflashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFlashSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
